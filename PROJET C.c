469910e6bd215a60a1ff6419e67b593d1ad76b54
Hatem Ben Akacha <hatem.benakacha@ensi-uma.tn>
2025-12-11 08:09:03 +0000

    TEST

--- PROJET C.c
+++ PROJET C.c
@@ -133,16 +133,14 @@ int prendre_prochaine_commande(Commande *out){
 int main(){
     init_file(&file_normale); init_file(&file_urgente);
     FILE *f=fopen("journal_robot.txt","w"); if(f){fprintf(f,"======= Journal Robot =======\n"); fclose(f);}
-    float duree_avancer = 5, vitesse_avancer = 1;
-    float duree_tourner = 2, vitesse_tourner = 1;
-    ajouter_commandes_initiales(duree_avancer,vitesse_avancer,duree_tourner,vitesse_tourner);
+    ajouter_commandes_initiales();
     printf("=== Simulation Robot Début ===\n");
     const int MAX_CYCLES=50; int cycle=0;
     Commande c_exec;
     while((!file_vide(&file_normale)||!file_vide(&file_urgente)) && cycle<MAX_CYCLES){
         if(capteur_detecte_obstacle()){
             printf("*** OBSTACLE DETECTE ***\n");
-            gerer_obstacle(duree_tourner,vitesse_tourner,duree_avancer,vitesse_avancer);
+            gerer_obstacle();
         }
 
         if(prendre_prochaine_commande(&c_exec)){
